69e3c9987b74baf18978c63918f46297706a5373
hieu.lehong <lehonghieu1412159@gmail.com>
2025-03-28 00:08:33 +0700

    removed redundant files

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-// import Navigation from '../pages/navigation'; // Adjust the path as necessary
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Navigation /> */}
       <Component {...pageProps} />
     </>
   );
